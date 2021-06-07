4a6e249dea32520f39a2cc7c1f0c9460f1d9203c
SmexyBouy <42865626+SmexyBouy@users.noreply.github.com>
2021-06-07 10:59:51 +0530

    Delete ProductMicroservice directory

--- ProductMicroservice/src/main/java/com/cognizant/repository/ProductRepository.java
+++
@@ -1,6 +0,0 @@
-package com.cognizant.repository;
-import org.springframework.data.repository.CrudRepository;
-import com.cognizant.model.Product;
-public interface ProductRepository extends CrudRepository<Product, String>
-{
-}
