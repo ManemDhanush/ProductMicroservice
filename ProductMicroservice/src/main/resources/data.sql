235a3244556f94ff2c080e5cda3ddc0fc1c8fdf5
SmexyBouy <42865626+SmexyBouy@users.noreply.github.com>
2021-06-08 13:55:15 +0530

    Add files via upload

---
+++ ProductMicroservice/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO PRODUCT VALUES ('1' , 'Comfortable Cotton T shirt for regular use', 'Polo-t-shirt', 5000, 5);  
+INSERT INTO PRODUCT VALUES ('2',  'Comfortable Cotton Pants for regular use', 'Polo-Pant', 2000, 4);
+INSERT INTO PRODUCT VALUES ('3' , 'Cotton Hankey',  'Hankey', 25,  1);
+INSERT INTO PRODUCT VALUES ('4', 'Top class Suit for wedding', 'Raymond-Suit', 25000, 5);      
+INSERT INTO PRODUCT VALUES ('5',  'Professional Tie', 'Raymond-Tie', 2500,3);
+INSERT INTO PRODUCT VALUES ('6', 'Nike Sports shoes for men', 'Nike-AirMax',  8000, 5);    
