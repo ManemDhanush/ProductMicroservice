1d4e9248969453f6a659c9fb4c0921c61c649547
SmexyBouy <42865626+SmexyBouy@users.noreply.github.com>
2021-06-17 11:01:46 +0530

    Update ProductService.java

--- ProductMicroservice/src/main/java/com/cognizant/service/ProductService.java
+++ ProductMicroservice/src/main/java/com/cognizant/service/ProductService.java
@@ -1,6 +1,8 @@
 package com.cognizant.service;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.cognizant.model.Product;
@@ -36,15 +38,15 @@ public class ProductService
 	public List<Product> getProductById(String id) 
 	{
 		List<Product> products = new ArrayList<Product>();
-		products.add(productRepository.findById(id).get());
-		return products;
+		productRepository.findAll().forEach(product -> products.add(product));
+		return products.stream().filter(c -> (c.getId().toLowerCase()).contains(id.toLowerCase())).collect(Collectors.toList());
 	}
 
 	public List<Product> getProductByName(String name) 
 	{
 		List<Product> products = new ArrayList<Product>();
-		productRepository.findAll().forEach(product -> { if(product.getName().toLowerCase().contains(name.toLowerCase()) == true) {products.add(product); } });
-		return products;
+		productRepository.findAll().forEach(product -> products.add(product));
+		return products.stream().filter(c -> (c.getName().toLowerCase()).contains(name.toLowerCase())).collect(Collectors.toList());
 	}
 	
 //	public void saveOrUpdate(Product product) 
