5bd383338d1ee298ea96847ec304e676bdbf9be3
SmexyBouy <42865626+SmexyBouy@users.noreply.github.com>
2021-06-18 10:27:15 +0530

    Delete ProductMicroserviceWithErrors directory

--- ProductMicroserviceWithErrors/src/main/java/com/cognizant/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cognizant.repository;
-import java.util.Optional;
-
-import org.springframework.data.repository.CrudRepository;
-import com.cognizant.model.Product;
-public interface ProductRepository extends CrudRepository<Product, String>
-{
-	Optional<Product> findById(String id);
-}
